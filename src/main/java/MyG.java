e5b18449d8a72347176f1c8b7e69738fc6d9e0c8
hiewtheg@gmail.com <tg@Ts-MacBook-Pro.local>
2025-08-28 11:52:46 +0800

    Updated chatbot intro name

--- src/main/java/MyG.java
+++ src/main/java/MyG.java
@@ -1,7 +1,7 @@
 public class MyG {
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n" +
-                " Hello! I'm [YOUR CHATBOT NAME]\n" +
+                " Hello! I'm MyG\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
