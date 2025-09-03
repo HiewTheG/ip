0a4d4550c72f59a35e737961f520bf8f2a9f01f6
hiewtheg@gmail.com <tg@Ts-MacBook-Pro.local>
2025-09-03 12:20:51 +0800

    Added echo function to chatbot

--- src/main/java/MyG.java
+++ src/main/java/MyG.java
@@ -1,11 +1,39 @@
+import java.util.Scanner;
+
 public class MyG {
     public static void main(String[] args) {
-        String intro = "____________________________________________________________\n" +
-                " Hello! I'm MyG\n" +
-                " What can I do for you?\n" +
+        // Response blocks
+        String intro =
                 "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+                " Hello! I'm MyG\n" +
+                " What can I do for you?\n";
+        String list =
+                "    ____________________________________________________________\n" +
+                "     list\n" +
+                "    ____________________________________________________________";
+        String blah =
+                "    ____________________________________________________________\n" +
+                "     blah\n" +
+                "    ____________________________________________________________\n";
+        String bye =
+                "    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________";
+
+        // Logic and flow control blocks
         System.out.println(intro);
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            String line = input.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                System.out.println(bye);
+                break;
+            } else if (line.equalsIgnoreCase("list")) {
+                System.out.println(list);
+            }  else if (line.equalsIgnoreCase("blah")) {
+                System.out.println(blah);
+            }
+        }
     }
 }
