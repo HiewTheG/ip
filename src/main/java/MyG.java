e949adbbde359e8d302b207f7a34ecfb7a040829
hiewtheg@gmail.com <tg@Ts-MacBook-Pro.local>
2025-09-16 18:17:15 +0800

    Added mark task as done function

--- src/main/java/MyG.java
+++ src/main/java/MyG.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MyG {
     public static void main(String[] args) {
-        // Response blocks
+        /** Response blocks */
         String intro =
                 "____________________________________________________________\n" +
                 " Hello! I'm MyG\n" +
@@ -18,11 +18,12 @@ public class MyG {
                 "     Bye. Hope to see you again soon!\n" +
                 "    ____________________________________________________________";
 
-        // Create storage for list of tasks
+        /** Storage for tasks */
         String[] tasks = new String[100];
+        Task[] taskObjects = new Task[100];
         int taskCount = 0;
 
-        // Logic and flow control blocks
+        /** Logic and flow control blocks */
         System.out.println(intro);
         Scanner input = new Scanner(System.in);
 
@@ -37,16 +38,34 @@ public class MyG {
                     System.out.println("No tasks yet.");
                 } else {
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                        System.out.println(" " + (i + 1) + ". [" + taskObjects[i].getStatusIcon() + "] " + tasks[i]);
                     }
                 }
                 System.out.println(listMsg);
             }  else if (line.equalsIgnoreCase("blah")) {
                 System.out.println(blah);
-            } else {
-                // store inputs
+            } else if (line.startsWith("mark")) {
+                try {
+                    int index = Integer.parseInt(line.split(" ")[1]) - 1;
+                    if (index >= 0 && index < taskCount) {
+                        taskObjects[index].markAsDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("  [" + taskObjects[index].getStatusIcon() + "] " + taskObjects[index].getDescription());
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println("Invalid task number.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid input for mark command.");
+                }
+            }
+            else {
+                // store new task
                 if (taskCount < 100) {
-                    tasks[taskCount++] = line;
+                    tasks[taskCount] = line;
+                    taskObjects[taskCount] = new Task(line);
+                    taskCount++;
                     System.out.println("____________________________________________________________");
                     System.out.println(" added: " + line);
                     System.out.println("____________________________________________________________");
