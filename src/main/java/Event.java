d68aad75dce6fff91359722498181d219ee73bb3
hiewtheg@gmail.com <tg@Ts-MacBook-Pro.local>
2025-09-18 00:06:15 +0800

    Added exception handling for Event class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -2,8 +2,11 @@ public class Event extends Task {
     protected String from;
     protected String to;
 
-    public Event(String description, String from, String to) {
+    public Event(String description, String from, String to) throws MyGException {
         super(description);
+        if (from == null || to == null) {
+            throw new MyGException("Oops, Event must have /from and /to dates/times.");
+        }
         this.from = from;
         this.to = to;
     }
