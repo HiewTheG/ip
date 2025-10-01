1eb23492d5bbc28e59501113663151af77c96061
hiewtheg@gmail.com <tg@Ts-MacBook-Pro.local>
2025-10-01 08:34:08 +0800

    Added toFileFormat() and getFileStatus() methods to Task for persistence

--- src/main/java/myg/Task.java
+++ src/main/java/myg/Task.java
@@ -28,6 +28,19 @@ public abstract class Task {
         isDone = false;
     }
 
+    /**
+     * Returns the status of the task as a file-friendly integer (1 for done, 0 for not done).
+     */
+    public String getFileStatus() {
+        return isDone ? "1" : "0";
+    }
+
+    /**
+     * Returns the task in the custom file format.
+     * The format should be: TYPE | IS_DONE (0/1) | DESCRIPTION [| EXTRA_DATA]
+     */
+    public abstract String toFileFormat();
+
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
